f9c36703eb2f5affe384428d299fa8a1b46e5aaa
Ioana Cioata <ioana.cioata26@gmail.com>
2017-11-23 15:54:19 +0200

    Fixed program bugs and modified automatons.

--- src/main/java/ro/ubb/lftc/Scanner.java
+++ src/main/java/ro/ubb/lftc/Scanner.java
@@ -10,8 +10,6 @@ import ro.ubb.lftc.model.programscanner.SymbolTable;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Scanner class uses all the other classes. In the constructor it initializes the CodingTable and creates the
@@ -57,13 +55,102 @@ public class Scanner {
 		symbolTable = new SymbolTable();
 		programInternalForm = new ProgramInternalForm();
 
-		String[] tokensVal = getProgramFromFile(filename);
+		String program = getProgramFromFile(filename);
 
-		int i = 0;
-		while (i < tokensVal.length) {
-			verifyToken(tokensVal[i]);
-			i++;
+		processProgram(program);
+	}
+
+	/**
+	 * @param program - the token to be checked
+	 * @throws CustomException - if it cannot identify a type for this token.
+	 *                         It allerts that there is a mistake in the written program from the file, that does
+	 *                         not respect the alphabet of this language
+	 */
+	private void processProgram(String program) throws CustomException {
+		String nextToken;
+		while (!program.isEmpty()) {
+			program = program.trim();
+			boolean found = false;
+			//look for symbols
+			nextToken = tokenFiniteAutomaton.getLongestPrefixForSequence(program);
+			if (!nextToken.isEmpty()) {
+				//look for keywords
+				if (codingTable.hasCode(nextToken)) {
+					found = true;
+					System.out.println("FOUND Keyword: " + nextToken);
+					programInternalForm.addValues(codingTable.getValueForCode(nextToken), null);
+				} else {
+					throw new CustomException("This is not in the coding table: " + nextToken);
+				}
+				program = program.substring(nextToken.length());
+			}
+
+			//look for identifiers
+			nextToken = identifierFiniteAutomaton.getLongestPrefixForSequence(program);
+			if (!nextToken.isEmpty()) {
+				found = true;
+				if (codingTable.hasCode(nextToken)) {
+					found = true;
+					System.out.println("FOUND Keyword: " + nextToken);
+					programInternalForm.addValues(codingTable.getValueForCode(nextToken), null);
+				} else {
+					System.out.println("FOUND Identifier: " + nextToken);
+					symbolTable.addValue(nextToken);
+					programInternalForm.addValues(codingTable.getValueForCode(IDENTIFIER), symbolTable.getPosition
+							(nextToken));
+				}
+				//remove this part from the token
+				program = program.substring(nextToken.length());
+			}
+			//look for constants - FLOAT
+			nextToken = floatFiniteAutomaton.getLongestPrefixForSequence(program);
+			if (!nextToken.isEmpty()) {
+				found = true;
+				System.out.println("FOUND Constant -  FLOAT: " + nextToken);
+				symbolTable.addValue(nextToken);
+				programInternalForm.addValues(codingTable.getValueForCode(CONSTANT), symbolTable.getPosition
+						(nextToken));
+				//remove this part from the token
+				program = program.substring(nextToken.length());
+			}
+			//look for constants - INTEGER
+			nextToken = integerFiniteAutomaton.getLongestPrefixForSequence(program);
+			if (!nextToken.isEmpty()) {
+				found = true;
+				System.out.println("FOUND Constant -  INTEGER: " + nextToken);
+				symbolTable.addValue(nextToken);
+				programInternalForm.addValues(codingTable.getValueForCode(CONSTANT), symbolTable.getPosition
+						(nextToken));
+				//remove this part from the token
+				program = program.substring(nextToken.length());
+			}
+
+			if (!found) {
+				throw new CustomException("Couldn't find a category for string '" + program + "'");
+			}
 		}
+	}
+
+	/**
+	 * Reads and splits the text in an array of string which were separated by space
+	 *
+	 * @param filename - the file from which it reads the program
+	 * @return an array of possible tokens
+	 * @throws CustomException in case of an IOException (cannot find or open the file)
+	 */
+	private String getProgramFromFile(String filename) throws CustomException {
+		StringBuilder program = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				program.append(line);
+				program.append(" ");
+			}
+		} catch (IOException e) {
+			throw new CustomException("Problems in reading the program :", e);
+		}
+		System.out.println(program);
+		return program.toString();
 	}
 
 	/**
@@ -83,115 +170,6 @@ public class Scanner {
 		if (!floatFiniteAutomaton.isDeterministic()) {
 			throw new CustomException("The finite automaton for FLOAT is not deterministic, It would cause errors!");
 		}
-	}
-
-	/**
-	 * Verifies tokens composed of a single word - simple keywords, constants(float or integer), identifiers
-	 * Exits the function if an empty string is found
-	 *
-	 * @param token - the token to be checked
-	 * @throws CustomException - if it cannot identify a type for this token.
-	 *                         It allerts that there is a mistake in the written program from the file, that does
-	 *                         not respect the alphabet of this language
-	 */
-	private void verifyToken(String token) throws CustomException {
-//		System.out.println("TOKEN: " + token);
-		String nextToken;
-		while (!token.isEmpty()) {
-			//look for keywords
-			boolean found = false;
-			if (codingTable.hasCode(token)) {
-				System.out.println("FOUND Keyword: " + token);
-				programInternalForm.addValues(codingTable.getValueForCode(token), null);
-				return;
-			}
-			//look for identifiers
-			nextToken = identifierFiniteAutomaton.getLongestPrefixForSequence(token);
-			if (!nextToken.isEmpty()) {
-				found = true;
-				System.out.println("FOUND Identifier: " + token);
-				symbolTable.addValue(nextToken);
-				programInternalForm.addValues(codingTable.getValueForCode(IDENTIFIER), symbolTable.getPosition
-						(nextToken));
-				//remove this part from the token
-				token = token.substring(nextToken.length());
-			}
-			//look for constants - FLOAT
-			nextToken = floatFiniteAutomaton.getLongestPrefixForSequence(token);
-			if (!nextToken.isEmpty()) {
-				found = true;
-				System.out.println("FOUND Constant -  FLOAT: " + nextToken);
-				symbolTable.addValue(nextToken);
-				programInternalForm.addValues(codingTable.getValueForCode(CONSTANT), symbolTable.getPosition
-						(nextToken));
-				//remove this part from the token
-				token = token.substring(nextToken.length());
-			}
-			//look for constants - INTEGER
-			nextToken = integerFiniteAutomaton.getLongestPrefixForSequence(token);
-			if (!nextToken.isEmpty()) {
-				found = true;
-				System.out.println("FOUND Constant -  INTEGER: " + nextToken);
-				symbolTable.addValue(nextToken);
-				programInternalForm.addValues(codingTable.getValueForCode(CONSTANT), symbolTable.getPosition
-						(nextToken));
-				//remove this part from the token
-				token = token.substring(nextToken.length());
-			}
-
-			if (!found) {
-				throw new CustomException("Couldn't find a category for string '" + token + "'");
-			}
-		}
-	}
-
-	/**
-	 * Reads and splits the text in an array of string which were separated by space
-	 *
-	 * @param filename - the file from which it reads the program
-	 * @return an array of possible tokens
-	 * @throws CustomException in case of an IOException (cannot find or open the file)
-	 */
-	private String[] getProgramFromFile(String filename) throws CustomException {
-		StringBuilder program = new StringBuilder();
-		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				program.append(line);
-				program.append(" ");
-			}
-		} catch (IOException e) {
-			throw new CustomException("Problems in reading the program :", e);
-		}
-		System.out.println(program);
-		return getTokens(program.toString());
-	}
-
-	/**
-	 * @param program - String (the program lines read from the file)
-	 * @return an array of strings representing possible tokens ( a token can be a sequence of letters and/or digits,
-	 * or the special signs from the coding table)
-	 * @throws CustomException - if cannot identify a token (that token is not accepted by the defined automaton)
-	 */
-	private String[] getTokens(String program) throws CustomException {
-		System.out.println("READING FROM FILE: ");
-		List<String> tokens = new ArrayList<>();
-		while (!program.isEmpty()) {
-			program = program.trim();
-			String nextToken = tokenFiniteAutomaton.getLongestPrefixForSequence(program);
-			//There should not be empty tokens, otherwise there is an error
-			if (!nextToken.isEmpty()) {
-				tokens.add(nextToken);
-				System.out.println("Next token: '" + nextToken + "'");
-				//remove from the beginning the next token
-				program = program.substring(nextToken.length());
-			} else {
-				throw new CustomException("Error reading the program from here:" + program);
-			}
-			//delete all spaces
-			program = program.trim();
-		}
-		return tokens.toArray(new String[0]);
 	}
 
 	public ProgramInternalForm getProgramInternalForm() {
